2ec86fc9c5224d8bbf2b21e9b951862f4dafb803
rand-work <86109238+ranganathsharma@users.noreply.github.com>
2024-03-01 19:16:35 +0000

    Final version

--- 7.py
+++ 7.py
@@ -1,7 +1,9 @@
 # 10001 st prime number
 
 # Given first six prime numbers
+import time
 
+start_time = time.time()
 prime_list = [2, 3, 5, 7, 11, 13]
 
 def prime_incr(prime_number):
@@ -10,22 +12,22 @@ def prime_incr(prime_number):
 def prime_check(prime_number, prime_list):
     for i in range(len(prime_list)):
         if prime_number%prime_list[i] == 0:
-            #raise ValueError
-            return True
-    #raise IndexError
-    return False
+            return False
+    return True
 
 # Mainloop
 count = 6
 temp_prime = prime_list[-1] + 1
 
 while count <= 10000:
-    a = prime_check(temp_prime, prime_list)
-    if a:
-        temp_prime = prime_incr(temp_prime)
-    else:
+    is_prime = prime_check(temp_prime, prime_list)
+    if is_prime:
         prime_list.append(temp_prime)
-        #print('Next prime number is', temp_prime)
         count += 1
+    else:
+        temp_prime = prime_incr(temp_prime)
+        #print('Next prime number is', temp_prime)
+end_time = time.time()
 
 print(prime_list[10000])
+print('The total run time is ', end_time - start_time)
